5463b56e7b393acb7b745de7a967f19e03790590
Bothriopsis <fisterniklas@outlook.com>
2024-12-10 13:00:35 +0100

    getting fast af

--- gameObjects/bar.py
+++ gameObjects/bar.py
@@ -9,7 +9,7 @@ class Bar:
         self.width = SCREEN_WIDTH/100
         self.height = SCREEN_HEIGHT/5
         self.color = color
-        self.dy = 10
+        self.dy = 20
     
     def draw(self, screen):
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
@@ -26,5 +26,5 @@ class Bar:
         if self.rect.colliderect(other.rect):
             if abs(self.rect.top - other.rect.bottom) <= abs(other.dy) or abs(self.rect.bottom - other.rect.top) <= abs(other.dy):
                 other.dy = -other.dy
-            if abs(self.rect.left - other.rect.right) <= abs(other.dy) or abs(self.rect.right - other.rect.left) <= abs(other.dy):
-                other.dx = -other.dx
+            if abs(self.rect.left - other.rect.right) <= abs(other.dx) or abs(self.rect.right - other.rect.left) <= abs(other.dx):
+                other.dx = -other.dx * 1.2
